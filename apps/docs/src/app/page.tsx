4c94caff16ae08c305c086f0b79ab7502dbd701d
CheolWoong Choi <bluesky4381@naver.com>
2024-04-23 21:04:55 +0900

    chore : 깃헙 액션 테스트 2

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -21,22 +21,22 @@ function Gradient({
 
 const LINKS = [
   {
-    title: "Docs",
+    title: "Title1",
     href: "https://turbo.build/repo/docs",
     description: "Find in-depth information about Turborepo features and API.",
   },
   {
-    title: "Learn",
+    title: "Title2",
     href: "https://turbo.build/repo/docs/handbook",
     description: "Learn more about monorepos with our handbook.",
   },
   {
-    title: "Templates",
+    title: "Title3",
     href: "https://turbo.build/repo/docs/getting-started/from-example",
     description: "Choose from over 15 examples and deploy with a single click.",
   },
   {
-    title: "Deploy",
+    title: "Title4",
     href: "https://vercel.com/new",
     description:
       "Instantly deploy your Turborepo to a shareable URL with Vercel.",
