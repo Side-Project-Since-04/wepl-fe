e354ff7bf30e1a4de5acd983b9eb36ac1d97c9dc
CheolWoong Choi <bluesky4381@naver.com>
2024-04-26 16:17:13 +0900

    fix : eslint parser 오류 해결

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ["@wepl/eslint-config/next.js"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
 };
