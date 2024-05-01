8c310d9fabdb4d6a959c8150b2d6dafeff9ba0ae
jun0811 <nate199458@gmail.com>
2024-05-01 15:42:57 +0900

    feat: test setting - sample 코드 작성

--- apps/client/app/sample/page.tsx
+++ apps/client/app/sample/page.tsx
@@ -1,3 +1,17 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function FSDPage() {
-  return <div>FSDPage</div>;
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3030/test').then((res) => setText(res.data)); //
+  }, [text]);
+
+  return (
+    <div>
+      FSDPage
+      <span>{text}</span>
+    </div>
+  );
 }
