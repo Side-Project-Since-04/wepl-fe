1c8ada58906f273c9e2ecdf32b01cb3cda300bfb
CheolWoong Choi <bluesky4381@naver.com>
2024-05-23 22:51:24 +0900

    feat : 앱 레이아웃 스타일링 & intro 페이지 추가

--- apps/client/src/app/appLayout.tsx
+++ apps/client/src/app/appLayout.tsx
@@ -1,10 +1,14 @@
-import "./lib/globals.css";
-import { QueryProvider } from "./queryProvider";
+import './lib/globals.css';
+import { QueryProvider } from './queryProvider';
 
 export function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <div className='m-auto border-l-2 border-r-2 border-indigo-500 max-w-[360px] h-screen'>
+      <QueryProvider>{children}</QueryProvider>
+    </div>
+  );
 }
