5df75e43e882bbb1454ab69d0ae9841427703c45
jun <nate199458@gmail.com>
2024-05-26 14:32:59 +1000

    Feature/turbo UI test (#29)
    * Prod 배포 (#19)
    
    * setting : App 이름 변경 & package scope 이름 변경
    
    * fix : eslint parser 오류 해결
    
    * setting : 빌드체크하는 Github 워크플로우 추가 & checkout 버전업
    
    * setting : build.sh 안의 레포지토리 이름 변경
    
    * setting : push가 되었을 때도 빌드체크하도록 처리
    
    * [setting] 기본 라이브러리 설치 & FSD에 맞게 폴더구조 구성 (#9)
    
    * setting : Root 레벨에서만 의존성 관리하기
    - 루트 디렉토리의 package.json에서만 의존성 설치
    
    * setting : FSD에 맞게 src 폴더 구성 & 라이브러리 적용
    
    * setting : 주석 제거
    
    * fix : 빌드 오류 수정
    - eslint rule을 추가해서 루트에서만 라이브러리를 관리하도록 처리했음
    - 빌드 시, import/no-extraneous-dependencies rule 때문에 에러 발생
    
    * setting : zustand 설치 & tsconfig의 paths 옵션 수정
    
    * feat: testing 환경
    - jest
    - msw
    
    * feat: test setting
    - sample 코드 작성
    
    * feat: test
    - package name 수정
    
    * feat: package.lock 수정
    
    * Revert "Feature/testing"
    
    * Feature/test setting (#14)
    
    * feat: testing setting
    - jest 설치 및 환경설정
    - msw 설치
    
    * feat: setting
    'use client' 추가
    
    * fix : turbo repo test (#15)
    
    * Fix/package (#16)
    
    * fix : turbo repo test
    
    * fix : test library root package로 이동
    
    * fix : root package
    - client test library root로 이동
    
    * Feature/kakao login (#17)
    
    * feat: kakao-login
    - nextauth provider
    - auth 로직
    
    * feat: kakao login
    - nextauth
    - provider
    - sample 코드 작성
    
    * Setting/deployment-noti
    
    * setting : 배포 noti 알리는 워크플로우 추가 & copy repo 워크플로우 수정
    
    * setting : 배포 noti 알리는 워크플로우 수정
    
    * setting : Copy-repo 워크플로우 수정 (dev, prod 모두)
    
    ---------
    
    Co-authored-by: jun0811 <nate199458@gmail.com>
    
    * Chore/메인 페이지 텍스트 변경 (#20)
    
    - 배포한 내용이 잘 반영되는지 체크
    
    * feat: turbo ui test setup
    
    * feat:
    packageManager 버전 수정
    
    * fix:잘못된 코드 수정
    
    * fix: card 삭제
    
    * fix: turbo eslint extend 추가
    
    ---------
    
    Co-authored-by: CheolWoongChoi <bluesky4381@naver.com>

--- apps/client/src/app/appLayout.tsx
+++ apps/client/src/app/appLayout.tsx
@@ -7,7 +7,7 @@ export function AppLayout({
   children: React.ReactNode;
 }): JSX.Element {
   return (
-    <div className='m-auto border-l-2 border-r-2 border-indigo-500 max-w-[360px] h-screen'>
+    <div className="m-auto border-l-2 border-r-2 border-indigo-500 max-w-[360px] h-screen">
       <QueryProvider>{children}</QueryProvider>
     </div>
   );
