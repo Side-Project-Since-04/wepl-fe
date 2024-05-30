b87a562f2b679b482b2b16ffafef9f5218d08c23
jun0811 <nate199458@gmail.com>
2024-05-30 10:03:24 +0900

    feat: test코드 colocation

--- apps/client/src/_pages/Login/index.test.tsx
+++ apps/client/src/_pages/Login/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { useRouter } from 'next/navigation';
 import { signIn, useSession } from 'next-auth/react';
 import LoginView from '@/src/_pages/Login';
