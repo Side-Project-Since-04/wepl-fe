7603991ee2b3611c31941ac24a7c6b1b6c5e3f31
CheolWoong Choi <bluesky4381@naver.com>
2024-06-03 14:17:20 +0900

    feat : jest-plugin-context 설치 (client 및 packages/ui) & 타입체크 적용

--- apps/client/jest.setup.js
+++ apps/client/jest.setup.js
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom';
+import 'jest-plugin-context';
+import 'jest-plugin-context/setup';
 
 import { server } from './mocks/server';
+
 beforeAll(() => server.listen());
 
 afterEach(() => server.resetHandlers());
