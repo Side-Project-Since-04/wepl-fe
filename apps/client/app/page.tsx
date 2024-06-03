a7f3f15e902d4d94c8d819a846223a56bdc50b37
jun <nate199458@gmail.com>
2024-06-03 10:32:07 +0900

    feat: main page (#40)
    * feat: main page
    디자인 반영

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 import React from 'react';
-import Image from 'next/image';
 import { Button } from '@wepl/ui/Button.tsx';
 import { useRouter } from 'next/navigation';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import tmpImage from './1.jpeg';
 
 export default function LoginPage(): JSX.Element {
   const router = useRouter();
@@ -24,13 +22,16 @@ export default function LoginPage(): JSX.Element {
     await signOut({ redirect: true, callbackUrl: '/' });
   };
   return (
-    <main className="flex flex-col items-center justify-center text-center h-full">
-      <Image src={tmpImage} width={192} height={73} alt="wepl" />
-      <h2>결혼도 관리가 필요하니까</h2>
-      <Button className="mt-4 w-60 p-5" onClick={handleKakaoBtn} variant="outline">
+    <main className="bg-primary-500 flex flex-col items-center justify-center text-center h-full">
+      <div className="flex items-center justify-center">
+        <img src="logo0.svg" />
+        <span className="text-5xl font-extrabold text-white font-sans">wepl</span>
+      </div>
+      <div className="text-white">결혼 준비에도 관리가 필요하니까</div>
+      <Button className="mt-8 w-72 p-5 h-14" onClick={handleKakaoBtn} variant="outline">
         카카오톡으로 시작하기
       </Button>
-      <Button className="mt-4 w-60 p-5" onClick={handleSignOut} variant="outline">
+      <Button className="mt-8 w-72 p-5 h-14" onClick={handleSignOut} variant="outline">
         로그아웃
       </Button>
     </main>
