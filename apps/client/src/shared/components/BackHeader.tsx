e5c9605b8aecbb64224b34eff79841afaeb7d214
CheolWoong Choi <bluesky4381@naver.com>
2024-06-11 23:26:03 +0900

    feat : BackHeaderProps 구현 - HeaderProps로부터 상속

--- apps/client/src/shared/components/BackHeader.tsx
+++ apps/client/src/shared/components/BackHeader.tsx
@@ -1,22 +1,24 @@
 'use client';
 
 import { Button } from '@ui/src/Button';
-import Header from '@ui/src/components/Header';
+import Header, { HeaderProps } from '@ui/src/components/Header';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
-const BackHeader = () => {
+interface BackHeaderProps extends HeaderProps {}
+
+const BackHeader = (props: BackHeaderProps) => {
   const router = useRouter();
 
   const LeftHeader = () => {
     return (
       <Button variant={'ghost'} className="p-0" onClick={() => router.back()}>
-        <img src="left_arrow.svg" />
+        <img src="/left_arrow.svg" />
       </Button>
     );
   };
 
-  return <Header left={<LeftHeader />} />;
+  return <Header left={<LeftHeader />} {...props} />;
 };
 
 export default BackHeader;
