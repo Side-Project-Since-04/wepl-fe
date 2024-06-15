2bbc77f9ba86a0994d2e5c92c6f296f0034cec2a
CheolWoongChoi <bluesky4381@naver.com>
2024-06-15 09:21:16 +0900

    Feature/Budget-list-2 (#53과 동일, 다시 Merge)  (#58)
    * feat : 예산 화면에 사용하는 헤더 컴포넌트 구현
    - BudgetHeader 컴포넌트로 추상화
    - 예산 화면 쪽에서 사용할 예정
    
    * fix : 예산 입력 컴포넌트들 위치 이동
    
    * fix : 예산 금액 리스트 페이지 구현
    
    * feat : 예산 리스트 상세 페이지 구현
    
    * fix : 대분류 상수 타입의 값을 소문자로 관리
    - ex) WEDDING = 'wedding'

--- apps/client/app/budget/input/page.tsx
+++ apps/client/app/budget/input/page.tsx
@@ -4,14 +4,13 @@ import BudgetDescription from '@/src/widgets/budget/input/BudgetDescription';
 import BudgetInput from '@/src/widgets/budget/input/BudgetInput';
 import { useToast } from '../../../../../packages/ui/src/Toast';
 import { useState } from 'react';
-import BackHeader from '@/src/shared/components/BackHeader';
-import { cn } from '@ui/lib/utils';
+import BudgetHeader from '@/src/widgets/budget/common/BudgetHeader';
 
 export default function BudgetInputPage() {
   const [budget, setBudget] = useState(0);
   const { toast } = useToast();
 
-  const isEnabled = budget > 0;
+  const isEnabledSave = budget > 0;
 
   // TODO
   const saveBudget = () => {
@@ -20,26 +19,19 @@ export default function BudgetInputPage() {
     toast({
       variant: 'success',
       title: '저장되었습니다.',
+      duration: 2000,
     });
-  };
-
-  const RightHeader = () => {
-    return (
-      <button
-        className={cn('text-button-lg', isEnabled ? 'text-primary-500' : 'text-gray-300')}
-        disabled={!isEnabled}
-        onClick={saveBudget}
-      >
-        저장
-      </button>
-    );
   };
 
   return (
     <main className="h-full">
-      <BackHeader right={<RightHeader />} />
-      <BudgetDescription />
-      <BudgetInput budget={budget} onChange={setBudget} />
+      <BudgetHeader isEnableSave={isEnabledSave} onSave={saveBudget} />
+      <section>
+        <BudgetDescription />
+      </section>
+      <section className="mt-40">
+        <BudgetInput budget={budget} onChange={setBudget} />
+      </section>
     </main>
   );
 }
