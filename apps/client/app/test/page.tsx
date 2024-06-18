64dec659bd5d33c4001f6b36394462c2e246e464
CheolWoong Choi <bluesky4381@naver.com>
2024-06-18 14:45:40 +0900

    feat : Icon 공통 컴포넌트 추가

---
+++ apps/client/app/test/page.tsx
@@ -0,0 +1,11 @@
+import Icon from '@ui/src/Icon';
+
+export default function page() {
+  return (
+    <main>
+      <div>
+        <Icon size={24} name="arrow-up" />
+      </div>
+    </main>
+  );
+}
