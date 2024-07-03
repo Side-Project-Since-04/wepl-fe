635b8457360d67e8b639a9bb4af7c38dca7826ab
CheolWoong Choi <bluesky4381@naver.com>
2024-07-03 23:06:09 +0900

    setting : next-router-mock 라이브러리 추가 & jest에 svg 설정 추가

--- apps/client/jest.config.mjs
+++ apps/client/jest.config.mjs
@@ -16,6 +16,7 @@ const config = {
     customExportConditions: [''],
   },
   moduleNameMapper: {
+    '(.*)(\\.svg|\\.svg\\?url)': '<rootDir>/svgMock.js',
     '@ui/(.*)$': '<rootDir>/../../packages/ui/$1',
   },
   setupFiles: ['<rootDir>/jest.polyfill.js'],
