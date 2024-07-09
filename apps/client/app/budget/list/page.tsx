b95fbbb2342ed2f3ea5bc2b61702962d5a7352d5
CheolWoong Choi <bluesky4381@naver.com>
2024-07-09 21:42:39 +0900

    feat : AsyncBoundary를 이용한 비동기코드 관리

--- apps/client/app/budget/list/page.tsx
+++ apps/client/app/budget/list/page.tsx
@@ -1,27 +1,24 @@
+'use client';
+
 import BudgetList from '@/src/widgets/budget/list/BudgetList';
 import BackHeader from '@/src/shared/components/BackHeader';
 import { BudgetAmount } from '@/src/widgets/budget/list/BudgetAmount';
-import { ErrorBoundary } from 'react-error-boundary';
-import { Suspense } from 'react';
+import AsyncBoundary from '@/src/shared/components/AsyncBoundary';
 
 export default function BudgetListPage() {
   return (
     <main>
       <BackHeader />
       <section className="py-16">
-        <ErrorBoundary fallback={<div>에러...</div>}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <BudgetAmount />
-          </Suspense>
-        </ErrorBoundary>
+        <AsyncBoundary>
+          <BudgetAmount />
+        </AsyncBoundary>
       </section>
       <section>
         <div className="py-16 text-gray-800 text-sub-title1">결혼 예산 리스트</div>
-        <ErrorBoundary fallback={<div>에러...</div>}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <BudgetList />
-          </Suspense>
-        </ErrorBoundary>
+        <AsyncBoundary>
+          <BudgetList />
+        </AsyncBoundary>
       </section>
     </main>
   );
