219a2897ee945a127a17e8a6952e29f0b7f278ea
CheolWoong Choi <bluesky4381@naver.com>
2024-07-25 15:48:51 +0900

    feat : 쿼리 API 오류 수정 & classNames 유틸 추가

--- apps/client/src/features/budget/queries.ts
+++ apps/client/src/features/budget/queries.ts
@@ -3,10 +3,10 @@ import { createQueryKeys } from '@lukemorales/query-key-factory';
 import { Budget } from './types';
 
 export const BudgetKeys = createQueryKeys('budget', {
-  getBudget: () => ({
-    queryKey: ['get'],
+  getBudget: {
+    queryKey: null,
     queryFn: () => BudgetClient.getBudget(),
-  }),
+  },
   updateBudget: (budget: Budget) => ({
     queryKey: [budget],
     queryFn: () => BudgetClient.updateBudget(budget),
