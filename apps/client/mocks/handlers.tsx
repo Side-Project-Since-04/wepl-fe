fb7dac53a4d1a6bd7f972836afab3a6bc77db631
CheolWoong Choi <bluesky4381@naver.com>
2024-07-25 14:55:57 +0900

    fix : auth API 파일을 shared/api로 이동

--- apps/client/mocks/handlers.tsx
+++ apps/client/mocks/handlers.tsx
@@ -2,4 +2,5 @@ import { handlers as classificationHandlers } from './handlers/classification';
 import { handlers as budgetHandlers } from './handlers/budget';
 import { handlers as weddingHandlers } from './handlers/wedding';
 
-export const handlers = [...classificationHandlers, ...budgetHandlers, ...weddingHandlers];
+export const handlers = [];
+// [...classificationHandlers, ...budgetHandlers, ...weddingHandlers];
