dd4cd3e9fbd3d00e77277649c28e800160bc07fe
CheolWoong Choi <bluesky4381@naver.com>
2024-07-25 15:47:31 +0900

    fix : 웨딩관련 type을 파일위치 이동

--- apps/client/src/features/wedding/types.ts
+++ apps/client/src/features/wedding/types.ts
@@ -1,5 +1,11 @@
-export interface WeddingInfo {
+export type WeddingInfoType = {
   weddingDate: string;
   weddingTime: string;
   weddingHall: string;
-}
+};
+
+export type WeddingBudgetInfoType = {
+  totalBudget: number;
+  totalSpending: number;
+  spendingPerBudget: number;
+};
