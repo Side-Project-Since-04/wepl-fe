277a71d8fb98768ac3e52a02450beb73a356d545
CheolWoong Choi <bluesky4381@naver.com>
2024-07-25 15:16:29 +0900

    chore : MSW 모킹해제 - 실제 API를 붙이고 있기 때문

--- apps/client/mocks/handlers.tsx
+++ apps/client/mocks/handlers.tsx
@@ -2,5 +2,4 @@ import { handlers as classificationHandlers } from './handlers/classification';
 import { handlers as budgetHandlers } from './handlers/budget';
 import { handlers as weddingHandlers } from './handlers/wedding';
 
-export const handlers = [];
-// [...classificationHandlers, ...budgetHandlers, ...weddingHandlers];
+export const handlers = [...classificationHandlers, ...budgetHandlers, ...weddingHandlers];
