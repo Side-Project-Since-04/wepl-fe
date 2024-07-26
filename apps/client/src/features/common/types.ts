e081fa4f72f88815989c42bc723f1856091b1c2d
CheolWoong Choi <bluesky4381@naver.com>
2024-07-26 19:02:41 +0900

    feat : ApiError 일단 구현

--- apps/client/src/features/common/types.ts
+++ apps/client/src/features/common/types.ts
@@ -5,3 +5,15 @@ export interface Pageable<T> {
   totalElements: number;
   totalPages: number;
 }
+
+/**
+ * TODO
+ * code, error는 정리 필요
+ */
+export interface ApiErrorType extends Error {
+  code: string;
+  error: string;
+  message: string;
+  status: number;
+  timestamp: string;
+}
