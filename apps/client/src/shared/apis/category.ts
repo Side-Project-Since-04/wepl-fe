afdd71755b0b85978be3877a1f6b2370644cd880
CheolWoong Choi <bluesky4381@naver.com>
2024-07-30 12:57:09 +0900

    fix : 예산 금액 저장 성공 후, 라우팅 & 쿼리데이터(서버 State)도 수정

--- apps/client/src/shared/apis/category.ts
+++ apps/client/src/shared/apis/category.ts
@@ -1,12 +1,12 @@
-import { ClassificationType } from '@/src/features/category/types';
+import type { ClassificationType } from '@/src/features/category/types';
+import type { Pageable } from '@fsd/features/common/types';
 import { axiosInstance } from '../config/axios';
-import { Pageable } from '@fsd/features/common/types';
 
 const URL_ROOT = '/category';
 
 export const CategoryClient = {
   getClassifications: async () => {
-    const { data } = await axiosInstance.get<Pageable<ClassificationType>>(URL_ROOT + '/classifications');
+    const { data } = await axiosInstance.get<Pageable<ClassificationType>>(`${URL_ROOT}/classifications`);
     return data;
   },
 };
