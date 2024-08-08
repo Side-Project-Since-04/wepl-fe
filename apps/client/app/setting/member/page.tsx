7b53164ee6843410b91c4b5d22dbdcfac3690bd2
CheolWoong Choi <bluesky4381@naver.com>
2024-08-08 14:22:53 +0900

    fix : 멤버관리에서 닉네임 변경하는 UI & 로직 추가

--- apps/client/app/setting/member/page.tsx
+++ apps/client/app/setting/member/page.tsx
@@ -3,6 +3,7 @@
 import { SubTitle2, TextBody2 } from '@ui/src/components/Text';
 import Image from 'next/image';
 import { Dialog } from '@ui/src/Dialog';
+import { useEffect, useRef, useState } from 'react';
 import BackHeader from '@/src/shared/components/BackHeader';
 import { WeplButton } from '@/src/shared/components/Button/WeplButton';
 import { classNames } from '@/src/shared/ui/utils';
@@ -10,10 +11,47 @@ import { useDeleteWeddingConnection } from '@/src/features/wedding/queries';
 import { useGetMember } from '@/src/features/member/queries';
 import { useInvitation } from '@/src/features/auth/hooks';
 
+const MAX_NICKNAME_LENGTH = 20;
+
 const SettingMemberPage = () => {
   const { mutate: deleteWeddingConnection } = useDeleteWeddingConnection();
   const { data: member } = useGetMember();
   const { sendInvitation } = useInvitation();
+
+  const [nickname, setNickname] = useState({
+    isChange: false,
+    value: member?.nickname || '',
+  });
+  const [openNicknameAlert, setOpenNicknameAlert] = useState(false);
+  const nicknameInputRef = useRef<HTMLInputElement>(null);
+
+  const handleClickChangeNickname = () => {
+    setNickname({
+      ...nickname,
+      isChange: true,
+    });
+  };
+
+  // TODO
+  const handleSaveNickname = () => {
+    if (!nickname.value) {
+      setOpenNicknameAlert(true);
+      return;
+    }
+
+    // api
+    // api success 시,
+    setNickname({
+      ...nickname,
+      isChange: false,
+    });
+  };
+
+  useEffect(() => {
+    if (nickname.isChange) {
+      nicknameInputRef.current?.focus();
+    }
+  }, [nickname.isChange]);
 
   return (
     <main>
@@ -30,10 +68,39 @@ const SettingMemberPage = () => {
 
       <section className={`my-32 ${classNames.pagePadding}`}>
         <div className="bg-gray-50 h-62 p-16 flex justify-between items-center">
-          <SubTitle2>{member?.nickname || ''}</SubTitle2>
-          <WeplButton className="bg-primary-400 hover:bg-primary-600 py-4 px-10 h-30 text-neutral-white" size="sm">
-            이름변경
-          </WeplButton>
+          {nickname.isChange ? (
+            <>
+              <SubTitle2>
+                <input
+                  className="bg-transparent"
+                  maxLength={MAX_NICKNAME_LENGTH}
+                  onChange={(e) => {
+                    setNickname({ ...nickname, value: e.target.value });
+                  }}
+                  ref={nicknameInputRef}
+                  value={nickname.value}
+                />
+              </SubTitle2>
+              <WeplButton
+                className="bg-primary-600 hover:bg-primary-700 py-4 px-10 h-30 text-neutral-white"
+                onClick={handleSaveNickname}
+                size="sm"
+              >
+                저장하기
+              </WeplButton>
+            </>
+          ) : (
+            <>
+              <SubTitle2>{nickname.value}</SubTitle2>
+              <WeplButton
+                className="bg-primary-400 hover:bg-primary-500 py-4 px-10 h-30 text-neutral-white"
+                onClick={handleClickChangeNickname}
+                size="sm"
+              >
+                이름변경
+              </WeplButton>
+            </>
+          )}
         </div>
         <div className="mt-12 h-62 p-16 bg-gray-50 flex justify-between items-center">
           <SubTitle2>{false ? <span>홍길동 짝꿍</span> : <span className="text-gray-400">짝꿍</span>}</SubTitle2>
@@ -68,6 +135,20 @@ const SettingMemberPage = () => {
           )}
         </div>
       </section>
+
+      {/* <Dialog
+        onClose={() => {
+          setOpenNicknameAlert(false);
+        }}
+        onSubmit={() => {
+          setOpenNicknameAlert(false);
+        }}
+        open={openNicknameAlert}
+        submitText="확인"
+        submitType="confirm"
+        subtitle="닉네임은 빈 값이 될 수 없습니다."
+        title=""
+      /> */}
     </main>
   );
 };
